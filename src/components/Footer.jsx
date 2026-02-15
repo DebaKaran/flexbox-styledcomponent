49efd6c3c84ac05b59b85886b0ba64b4eb50b7b9
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-15 17:38:54 +0530

    feat(footer): implement styled footer with navigation and social links
    - Create StyledFooter, FooterNavList, and FooterNavItem components
    - Add styled image for social icons
    - Implement semantic navigation structure
    - Integrate social media icon links
    - Compose footer layout within App

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FooterNavItem, FooterNavList, StyledFooter } from './styled/Footer.styled'
+import { FooterNavItem, FooterNavList, FooterSocialIcons, StyledFooter } from './styled/Footer.styled'
 import FooterLink from './FooterLink'
 
 const Footer = () => {
@@ -18,6 +18,34 @@ const Footer = () => {
             <FooterNavItem> 
                 <FooterLink href="register.html">
                     Register
+                </FooterLink>
+            </FooterNavItem>
+        </FooterNavList>
+
+        <FooterNavList>
+             <FooterNavItem> 
+                <FooterLink href="https://www.instagram.com/">
+                    <FooterSocialIcons src="images/instagram.svg" alt="instagram logo" />
+                </FooterLink>
+            </FooterNavItem>
+            <FooterNavItem> 
+                <FooterLink href="https://www.facebook.com/">
+                    <FooterSocialIcons src="images/facebook.svg" alt="facebook logo"/>
+                </FooterLink>
+            </FooterNavItem>
+            <FooterNavItem> 
+                <FooterLink href="https://www.tiktok.com/">
+                    <FooterSocialIcons src="images/tiktok.svg" alt="tiktok logo"/>
+                </FooterLink>
+            </FooterNavItem>
+            <FooterNavItem> 
+                <FooterLink href="https://www.linkedin.com/">
+                    <FooterSocialIcons src="images/linkedin.svg" alt="linkedin logo"/>
+                </FooterLink>
+            </FooterNavItem>
+            <FooterNavItem> 
+                <FooterLink href="https://www.youtube.com/">
+                    <FooterSocialIcons src="images/youtube.svg" alt="youtube logo"/>
                 </FooterLink>
             </FooterNavItem>
         </FooterNavList>
--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -8,3 +8,6 @@ export const FooterNavList = styled.ul``
 
 export const FooterNavItem = styled.li``
 
+export const FooterSocialIcons = styled.img`
+    width: 20px;
+`
