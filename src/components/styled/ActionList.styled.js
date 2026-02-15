f6afeaf97ab587b597e17aefa0ae77a0fb2716a0
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-16 01:32:59 +0530

    - Reset list-style, padding, and margin on styled ul components - Remove unwanted bullet points

--- src/components/styled/ActionList.styled.js
+++ src/components/styled/ActionList.styled.js
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const ActionList = styled.ul``
 
-export const ActionItem = styled.li``
+export const ActionItem = styled.li`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+`
