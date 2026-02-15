e9f3c5f6c2990c473ba621a65a36eab5030f6e29
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-15 18:06:14 +0530

    refactor(footer): extract navigation and social links into data arrays
    - Move footer navigation links into footerLinks array
    - Move social media links into footerSocialLinks array
    - Replace hardcoded JSX with map-based rendering
    - Improve scalability and maintainability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,53 +1,40 @@
 import { FooterNavItem, FooterNavList, FooterSocialIcons, StyledFooter } from './styled/Footer.styled'
 import FooterLink from './FooterLink'
 
+const footerLinks = [
+  { label: "Home", href: "index.html" },
+  { label: "Login", href: "login.html" },
+  { label: "Register", href: "register.html" },
+];
+
+const footerSocialLinks = [
+    {href: "https://www.instagram.com/", src: "images/instagram.svg", alt: "instagram logo"},
+    {href: "https://www.facebook.com/", src:"images/facebook.svg", alt:"facebook logo"},
+    {href: "https://www.tiktok.com/", src:"images/tiktok.svg", alt: "tiktok logo"},
+    {href: "https://www.linkedin.com/", src: "images/linkedin.svg", alt:"linkedin logo"},
+    {href: "https://www.youtube.com/", src: "images/youtube.svg", alt:"youtube logo"},
+]
 const Footer = () => {
   return (
     <StyledFooter>
         <FooterNavList>
-            <FooterNavItem> 
-                <FooterLink href="index.html">
-                    Home
+            {footerLinks.map(link => (
+            <FooterNavItem key={link.href}> 
+                <FooterLink href={link.href}>
+                    {link.label}
                 </FooterLink>
             </FooterNavItem> 
-            <FooterNavItem> 
-                <FooterLink href="login.html">
-                    Login
-                </FooterLink>
-            </FooterNavItem>
-            <FooterNavItem> 
-                <FooterLink href="register.html">
-                    Register
-                </FooterLink>
-            </FooterNavItem>
+        ))}
         </FooterNavList>
 
         <FooterNavList>
-             <FooterNavItem> 
-                <FooterLink href="https://www.instagram.com/">
-                    <FooterSocialIcons src="images/instagram.svg" alt="instagram logo" />
+            {footerSocialLinks.map(link => (
+            <FooterNavItem key={link.href}> 
+                <FooterLink href={link.href}>
+                    <FooterSocialIcons src={link.src} alt={link.alt}/>
                 </FooterLink>
-            </FooterNavItem>
-            <FooterNavItem> 
-                <FooterLink href="https://www.facebook.com/">
-                    <FooterSocialIcons src="images/facebook.svg" alt="facebook logo"/>
-                </FooterLink>
-            </FooterNavItem>
-            <FooterNavItem> 
-                <FooterLink href="https://www.tiktok.com/">
-                    <FooterSocialIcons src="images/tiktok.svg" alt="tiktok logo"/>
-                </FooterLink>
-            </FooterNavItem>
-            <FooterNavItem> 
-                <FooterLink href="https://www.linkedin.com/">
-                    <FooterSocialIcons src="images/linkedin.svg" alt="linkedin logo"/>
-                </FooterLink>
-            </FooterNavItem>
-            <FooterNavItem> 
-                <FooterLink href="https://www.youtube.com/">
-                    <FooterSocialIcons src="images/youtube.svg" alt="youtube logo"/>
-                </FooterLink>
-            </FooterNavItem>
+            </FooterNavItem> 
+        ))}
         </FooterNavList>
     </StyledFooter>
   )
