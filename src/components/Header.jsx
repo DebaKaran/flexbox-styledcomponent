4c4f2ffa8e7aed2965a3cba8e2e72292aacbe70b
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-15 13:35:53 +0530

    feat(layout): compose Header with Navbar and Hero section
    - Combine NavBar and Section components
    - Establish top-level header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import NavBar from './NavBar'
+import Section from './Section'
+
+const Header = () => {
+  return (
+    <>
+        <NavBar />
+        <Section />
+    </>
+  )
+}
+
+export default Header
