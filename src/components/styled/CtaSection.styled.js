9e9d746cb3429bc4602f68282518f4032aaf2a2e
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-17 15:23:05 +0530

    feat(cta): center CTA card using flexbox on section wrapper
    - Convert CtaSectionWrapper into flex container
    - Add flex-direction: row and justify-content: center
    - Remove margin: 0 auto from CtaCard
    - Make CtaCard width: 100% with max-width constraint
    - Achieve horizontal centering via flexbox instead of margin

--- src/components/styled/CtaSection.styled.js
+++ src/components/styled/CtaSection.styled.js
@@ -2,15 +2,19 @@ import styled from "styled-components";
 
 export const CtaSectionWrapper = styled.section`
   margin-bottom: 30rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 export const CtaCard = styled.div`
   text-align: center;
   background-color: #339af0;
   max-width: 1000px;
+  width: 100%;
   padding: 10rem 20rem;
   border-radius: 20px;
-  margin: 0 auto;
+  /* margin: 0 auto; */
 `;
 
 export const CtaHeading = styled.h2`
