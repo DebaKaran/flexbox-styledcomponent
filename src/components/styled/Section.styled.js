3ea96ddcb3a156f2958ce72f57d0f0b95086cb5a
Deba Karan Agrawala <karan.deba@gmail.com>
2026-02-17 13:57:37 +0530

    feat(hero): center multi-line header text in hero section
    - Add text-align: center to StyledHeader
    - Ensure line break content aligns centrally
    - Fix left alignment issue caused by <br />

--- src/components/styled/Section.styled.js
+++ src/components/styled/Section.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const StyledSection = styled.section`
     min-height: 400px;
-    /* text-align: center; */
     margin-bottom: 10rem;
     height: calc(100vh - 300px);
     display: flex;
@@ -13,5 +12,6 @@ export const StyledSection = styled.section`
 
 export const StyledHeader = styled.h1`
     margin-bottom: 1rem;
+    text-align: center;
 `;
 
